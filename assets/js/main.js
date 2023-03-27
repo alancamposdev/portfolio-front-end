a61891bc9c5ae93a1e3bb4cc3f0a530e5c8ac725
AlanCampos <alancamposdeveloper@gmail.com>
2023-03-27 17:33:10 -0400

    trabalhando no header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,37 @@
 // Links ativos dinamicos
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('.header_link');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".header_link");
 
-
-window.onscroll = () =>{
-  sections.forEach( sec => {
+window.onscroll = () => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');   
-    
-    if( top >= offset && top < offset + height){
+    let id = sec.getAttribute("id");
 
-      navLinks.forEach(links => {
-        
-        links.classList.remove('active');
-        document.querySelector('.header_link[href*='+ id +']').classList.add('active');
-
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(".header_link[href*=" + id + "]")
+          .classList.add("active");
       });
     }
-  })
-}
+  });
+};
 
 // ativas o btn mobile atraves da classse 'active'
-let btnMobile = document.querySelector('.btn-mobile');
-let menu = document.querySelector('.header_nav');
+let btnMobile = document.querySelector(".btn-mobile");
+let menu = document.querySelector(".header_nav");
 
-btnMobile.addEventListener('click', (event) => {
-  menu.classList.toggle('active');
-  const active =  menu.classList.contains('active')
-  event.currentTarget.setAttribute('aria-expanded',active);
-  if(active) {
-    event.currentTarget.setAttribute('aria-label','Fechar Menu');
-  }else{
-    event.currentTarget.setAttribute('aria-label','Abrir Menu');
+btnMobile.addEventListener("click", (event) => {
+  menu.classList.toggle("active");
+
+  const active = menu.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
- 
-})
+});
