8fb4b4a670383f0f6d6439031e46073b86390943
AlanCampos <alancamposdeveloper@gmail.com>
2023-03-27 13:08:57 -0400

    add tamanhos nas seções e melhorado o header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
-// alert('testado') 
-console.log('no console');
+// Links ativos dinamicos
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header_link');
+
+
+window.onscroll = () =>{
+  sections.forEach( sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');   
+    
+    if( top >= offset && top < offset + height){
+
+      navLinks.forEach(links => {
+        
+        links.classList.remove('active');
+        document.querySelector('.header_link[href*='+ id +']').classList.add('active');
+
+      });
+    }
+  })
+}
